c8a1db152db0d460d2656a458181561d7a81fdbb
YuyuanDing <yuyuanding0@gmail.com>
2019-05-09 14:51:31 +1200

    1

--- src/app/Comments-Form/Comments-Form.component.ts
+++ src/app/Comments-Form/Comments-Form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators  } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-Comments-Form',
@@ -7,20 +8,24 @@ import { FormGroup, FormBuilder, Validators  } from '@angular/forms';
   styleUrls: ['./Comments-Form.component.css']
 })
 export class CommentsFormComponent implements OnInit {
-  myForm:FormGroup
-  submitted=false
-  success=false
-  errorMessage:string
-  constructor(public fb:FormBuilder) { }
+  myForm: FormGroup
+  submitted = false
+  success = false
+  errorMessage: string
+  
+  successData: any
+  url = "./assets/data/comments.json"
+  constructor(public fb: FormBuilder, private http: HttpClient) { }
 
   ngOnInit() {
     this.createForm();
+
   }
 
-  createForm(){
+  createForm() {
     this.myForm = this.fb.group({
-      title:['',[Validators.required, Validators.minLength(2)]],
-      comments:['',[Validators.required,Validators.minLength(10), Validators.maxLength(200)]]
+      title: ['', [Validators.required, Validators.minLength(2)]],
+      comments: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(200)]]
     })
   }
 
@@ -28,18 +33,38 @@ export class CommentsFormComponent implements OnInit {
     this.submitted = true;
     console.log(this.myForm.value)
     // stop here if form is invalid
-    
-    
+
+
     if (this.myForm.invalid ) {
       console.log(this.myForm)
-      this.errorMessage='Please fill all input.'
-      this.success=false; 
+      this.errorMessage = 'Please fill all input.'
+      this.success = false;
       return
-        }
-    else{
+    }
+    else {
       console.warn('success')
-      this.success=true;
-   
-        }
+      this.success = true;
+      this.pushData()
+    }
+  }
+
+
+  pushData(){
+    this.postdata(this.myForm).subscribe(
+      (data) => {
+      this.successData = data;
+        console.log('success', data)
+      },
+      (err) => {
+      this.errorMessage = err;
+        console.log('fail', err)
+      }
+    )
+  }
+
+  postdata(myForm) {
+    return this.http.post(this.url, myForm)
+
+
   }
 }
--- src/app/display-details/display-details.component.ts
+++ src/app/display-details/display-details.component.ts
@@ -16,18 +16,18 @@ export class DisplayDetailsComponent implements OnInit {
   constructor(
     private http: HttpClient,
     private route: ActivatedRoute) { }
-    // 
+    
   ngOnInit() {
     console.log(this.route)
-    this.route.params.subscribe(params => {    //get route params 
+    this.route.params.subscribe(params => {//get route params 
       console.log(params)
       this.routeId = +params['id']; // (+) converts string 'id' to a number; assign route params value to local to get data
       this.apiProcess(this.routeId)// pass required id to get data
-      // In a real app: dispatch action to load the details here.
+     
    })
   }
 
-  // get data for the required id from api
+  // get data for the required id from api, subscribe to observable data get from data source, response if get data, error message if no data
   apiProcess(dataId){
     this.callApi(dataId).subscribe(
       (res) => (console.log(res), this.Comments=res),
@@ -35,7 +35,7 @@ export class DisplayDetailsComponent implements OnInit {
     )
   }
 
-  //link to service
+  //send http request to get data from data source, return observable data
   callApi(id){
     return this.http.get('https://jsonplaceholder.typicode.com/comments/'+ id)
   }
